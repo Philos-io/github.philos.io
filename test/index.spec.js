25eedd817a0e577e1417c826654830c6fe4c3ede
davy engone <davy@philos.io>
2015-08-07 23:46:35 +0200

    initial commit

---
+++ test/index.spec.js
@@ -0,0 +1,9 @@
+var assert  = require('assert');
+
+describe('Feature', function() {
+	describe('Scenario 1', function() {
+		it('should behave...', function() {
+			assert(false);		
+		});
+	});
+});
