7bf0530db138247f5171247e8bdd73639f92f232
davy engone <davy@philos.io>
2015-09-29 18:10:48 +0200

    added few tests

---
+++ test/index.spec.js
@@ -0,0 +1,59 @@
+
+
+describe('Testing MainController', function () {
+	var main, scope;
+
+	beforeEach(module('App'));
+
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+
+		main = $controller('MainController', {
+			$scope: scope
+		});
+
+	}));
+
+	it('should have a property title', function () {
+		console.log(main.title);
+
+		expect(main.title).toEqual('title');
+	});
+
+});
+
+
+describe('Testing a service', function(){
+
+	beforeEach(module('App'));
+
+	var factory, httpBackend;
+
+	beforeEach(inject(function(_MainFactory_, $httpBackend){
+		factory = _MainFactory_;
+		httpBackend = $httpBackend;
+	}));
+
+	it('should return an object', function(){
+		httpBackend.whenGET('/user')
+			.response(200, {login: 'davyengone'});
+
+		httpBackend.flush();
+	});
+
+});
+
+describe('Testing directive', function(){
+	var scope, element;
+
+	beforeEach(module('App'));
+
+	beforeEach(inject(function($rootScope, $compile){
+
+		scope = $rootScope.$new();
+
+		element = '<>'
+
+	}))
+
+});
